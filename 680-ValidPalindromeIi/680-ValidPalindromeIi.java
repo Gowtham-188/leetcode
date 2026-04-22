719c8ad9318fcb3febd7cc616308e5183c105dc9
Gowtham-188 <gowthamganeshan188@gmail.com>
2026-04-22 15:46:23 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.java
@@ -0,0 +1,31 @@
+// Last updated: 4/22/2026, 3:46:22 PM
+class Solution {
+    public boolean validPalindrome(String s) {
+
+        int left=0;
+        int right=s.length()-1;
+
+       
+    while(left<right)
+    {
+        if(s.charAt(left)!=s.charAt(right))
+        {
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1); 
+        }
+       
+         left++;
+         right--; 
+
+        }
+
+         return true;
+    }
+        public boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
