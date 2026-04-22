b4d597ddf6fa2e1055b5674430b060c3591de7b6
Gowtham-188 <gowthamganeshan188@gmail.com>
2026-04-22 15:47:30 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,23 @@
+// Last updated: 4/22/2026, 3:47:29 PM
+class Solution {
+    public int rob(int[] nums) {
+        
+    int n=nums.length;
+
+    if(n==1)
+    {
+        return nums[0];
+    }
+    int []  arr=new int [n];
+
+    arr[0]=nums[0];
+    arr[1]=Math.max(nums[0],nums[1]);
+
+    for(int i=2;i<n;i++)
+    {
+        arr[i]=Math.max(arr[i-1],nums[i]+arr[i-2]);
+    }
+    return arr[n-1];
+
+}
+}
