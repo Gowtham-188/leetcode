b9c6adc87e23352d52bbf65a784b38903215cd10
Gowtham-188 <gowthamganeshan188@gmail.com>
2026-04-22 15:45:50 +0530

    Create 1153-ProductSalesAnalysisI

---
+++ 1153-ProductSalesAnalysisI/1153-ProductSalesAnalysisI.sql
@@ -0,0 +1,6 @@
+-- Last updated: 4/22/2026, 3:45:49 PM
+# Write your MySQL query statement below
+
+select p.product_name ,s.year,s.price 
+from sales  s join product p
+on s.product_id = p.product_id ;
