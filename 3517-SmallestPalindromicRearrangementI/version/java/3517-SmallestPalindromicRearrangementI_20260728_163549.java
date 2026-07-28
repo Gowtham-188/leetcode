9420f9a63f2b41a7a4ffae0222afc33654b9bb5a
Gowtham-188 <gowthamganeshan188@gmail.com>
2026-07-28 16:35:51 +0530

    Create 3517-SmallestPalindromicRearrangementI

---
+++ 3517-SmallestPalindromicRearrangementI/version/java/3517-SmallestPalindromicRearrangementI_20260728_163549.java
@@ -0,0 +1,20 @@
+// Last updated: 7/28/2026, 4:35:49 PM
+1class Solution {
+2    public String smallestPalindrome(String s) {
+3
+4        int n = s.length() ;
+5 
+6        char[] arr =s.substring(0,n/2).toCharArray();
+7        Arrays.sort(arr);
+8
+9         String res = new String(arr) + (n%2 == 1 ? s.charAt(n/2) : "") + new StringBuilder(new String(arr)).reverse();
+10
+11         
+12
+13        
+14
+15        return res;
+16
+17
+18    }
+19}
